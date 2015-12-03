489e5b1c4d6aee71483d26f0dc2c41f833cddd87
Christopher Roberson <croberson1988@gmail.com>
2015-12-03 13:33:04 -0600

    Update config.php

--- system/config.php
+++ system/config.php
@@ -7,7 +7,11 @@
   to have a javascript 'Settings' tab which will use ajax to manipulate server XML (or other format). Each settings 
   will trigger the appropriate PHP script. Only specific parameters can be passed into the configuration files, obv 
   because they need to read specific character sequences or strings to function! Some settings will be configured by system 
-  events as well, esp. to avoid failures in execution.
+  events as well, esp. to avoid failures in execution. 
+  
+  
+  Dynamic configurations are included, such as the current display pattern (which is also stored in the session). Requests will be 
+  accepted by this class for certain information otherwise unavailable to javascript, but not including animations, speech, et
   */
   
  
