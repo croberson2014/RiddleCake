a300f0590c78dc68e6a5d92bb5422de6bb32d03c
Christopher Roberson <croberson1988@gmail.com>
2015-12-03 13:11:19 -0600

    Update config.php

--- system/config.php
+++ system/config.php
@@ -1,3 +1,18 @@
 <?php 
-
+  include 'uiconfig.php';
+  public class Configuration{
+  /*    
+  version : 1.0 
+  description : This class will allow manipulation of configuration content via an OO class. The current plan is 
+  to have a javascript 'Settings' tab which will use ajax to manipulate server XML (or other format). Each settings 
+  will trigger the appropriate PHP script. Only specific parameters can be passed into the configuration files, obv 
+  because they need to read specific character sequences or strings to function! Some settings will be configured by system 
+  events as well, esp. to avoid failures in execution.
+  */
+  
+ 
+    
+ 
+  
+  }
 ?>
