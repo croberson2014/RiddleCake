b8aba7953d405a3e1fc3021100d922e3ecf48129
Christopher Roberson <croberson1988@gmail.com>
2015-12-04 14:36:23 -0600

    Update ncs.php

--- system/ncs.php
+++ system/ncs.php
@@ -2,5 +2,5 @@
 //this is necessary to avoid coupling of all the classes necessary, and it works with the resolution engine and ui configurations most
 //It's a hub... a gateway of sorts. 
 <?php 
-  include('config.php'); include('engine/resolution_engine.php'); 
+  include('config.php'); include('dms.php'); include('engine/resolution_engine.php'); 
 ?>
