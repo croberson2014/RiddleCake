f021d7cec43919034d15d8dede2a4181665267bb
Christopher Roberson <croberson1988@gmail.com>
2015-12-04 13:26:34 -0600

    Create animation_engine.php

---
+++ system/engine/animation_engine.php
@@ -0,0 +1,9 @@
+<?php 
+/* 
+Version : 1.0 
+Description : Receives requests for sequences and information about them. Will have access to image files that will be displayed in a 
+variety of circumstances, including for the various cakes and any animations that occur as well. 
+*/
+
+
+?> 
