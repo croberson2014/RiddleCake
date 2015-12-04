9bfee4a9f7ee3b36674e83a6384c772b3f5e35bd
Christopher Roberson <croberson1988@gmail.com>
2015-12-04 12:06:35 -0600

    Create sentiment_engine.php

---
+++ system/engine/sentiment_engine.php
@@ -0,0 +1,10 @@
+public class SentimentEngine{
+
+/*
+Version 1 : 
+Description : This handles the 'rankings' of the severity and types of responses based on the character of your cake 
+each cake will respond differently and this engine will assist in it. 
+
+*/
+
+}
