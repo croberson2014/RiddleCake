b3680cbbc6e0a211293d33034fc941e6fc43c994
Christopher Roberson <croberson1988@gmail.com>
2015-12-04 13:31:18 -0600

    Create communication_engine.php

---
+++ system/engine/communication_engine.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+  Version : 1.0 
+  Description : Beneath the surface of this joyous cake is a machine of utter chaos and desctruction, boasting a total of 5 engines 
+  at this point, and maybe more. This particular engine is responsible for sending linked lists of messages to the callout bubble. 
+  Information about the area and the duration of a message will be present (probably the number of frames). 
+*/
+
+?>
