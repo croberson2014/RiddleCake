14768042f4f52351e9e646eb60c8fb3d764ada95
Christopher Roberson <croberson1988@gmail.com>
2015-12-04 12:25:19 -0600

    Create ncs.php

---
+++ system/ncs.php
@@ -0,0 +1,2 @@
+//nodal communication system accepts requests with overloaded parameters and will direct the required operations and return results 
+//this is necessary to avoid coupling of all the classes necessary, and it works with the resolution engine and ui configurations most
