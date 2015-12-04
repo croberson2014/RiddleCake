c1a11cf4e0039e424e261f0ec13eff0a864c3cc3
Christopher Roberson <croberson1988@gmail.com>
2015-12-04 14:48:17 -0600

    Update resolution_engine.php

--- system/engine/resolution_engine.php
+++ system/engine/resolution_engine.php
@@ -1,4 +1,5 @@
-//this could be an information expert, should be easy to decouple of necessary 
+//this could be an information expert, should be easy to decouple of necessary
+// sentiment -> recall -> communication -> animation 
 <?php
 
 ?> 
