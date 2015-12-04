12e4ecc271873988379ef52124350358c7d9441f
Christopher Roberson <croberson1988@gmail.com>
2015-12-04 12:28:28 -0600

    Update ncs.php

--- system/ncs.php
+++ system/ncs.php
@@ -1,2 +1,6 @@
 //nodal communication system accepts requests with overloaded parameters and will direct the required operations and return results 
 //this is necessary to avoid coupling of all the classes necessary, and it works with the resolution engine and ui configurations most
+//It's a hub... a gateway of sorts. 
+<?php 
+  include('config.php'); include('engine/resolution_engine.php'); 
+?>
