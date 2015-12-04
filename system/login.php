00c4c5a624ce8ca890fb77f6a38e388233e2629a
Christopher Roberson <croberson1988@gmail.com>
2015-12-04 14:38:44 -0600

    Create login.php

---
+++ system/login.php
@@ -0,0 +1,7 @@
+<?php 
+/*
+Version : 1.0 
+Description : Using MySQL direct in order to login with credentials. The login simply checks the credentials and updates the 
+session appropriately. 
+*/ 
+?> 
