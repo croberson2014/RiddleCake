dc25c6bfc11cbb720abbaa0ffcef786165031b27
Christopher Roberson <croberson1988@gmail.com>
2015-12-04 14:45:10 -0600

    Update recall_engine.php

--- system/engine/recall_engine.php
+++ system/engine/recall_engine.php
@@ -1,5 +1,9 @@
 //short for memory recall, this engine deals with tracking changes to user activity over time
-//this engine does not handle tracking user preferences 
+
 <?php 
+
+//how many times the settings are changed, it has counters and it stores them in a file for each user (like the configuration settings)
+//if the user has already completed the introduction interview (those results are also stored and can be accessed by this)
+//has access to user preferences, links clicked, etc. how many times the pages were viewed
 
 ?> 
