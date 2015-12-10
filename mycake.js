3b7d7849b7afab27affd37b573ec47944cac0d1c
Christopher Roberson <croberson1988@gmail.com>
2015-12-10 12:46:59 -0600

    Update mycake.js

--- mycake.js
+++ mycake.js
@@ -1,1 +1,7 @@
-
+function processContext(data){
+	//alert(data);
+	var process = jQuery.parseJSON(data);
+	
+	setInterval(demoCake, 100, 1, process.sequence, process.message); 
+	//for each screen, process the slides for that screen, wait to be synced, update the next round
+}
