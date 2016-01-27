f871db4bd0789625cc394b5dbb4f8e0fa01241d8
RussWilkie <russwilkie@gmail.com>
2016-01-26 22:01:00 -0600

    Create form.php

---
+++ lib/form.php
@@ -0,0 +1,14 @@
+<html>
+<head></head>
+<body>
+<form>
+<p>
+Username: <input type="text" name="Username" value="" size="25" maxlength="30" />
+</p>
+<p>
+Password: <input type="password" name="Password" value="" size="24" maxlength="30"/>
+</p>
+<p><input type="submit" name="submit" value="Submit" /></p>
+</form>
+</body>
+</html>
