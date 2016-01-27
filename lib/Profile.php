f52c2636bcf0a3590a3a0d543b2cb46b28442560
RussWilkie <russwilkie@gmail.com>
2016-01-26 22:01:21 -0600

    Create Profile.php

---
+++ lib/Profile.php
@@ -0,0 +1,13 @@
+<html>
+<head><h1>User Profile</h1></head>
+<body>
+<p>Name:<input type="text" name="Name" value="" size="25" maxlength="30" /></p>
+<p>Address:<input type="text" name="Address" value="" size="50" maxlength="125" /></p>
+<p><input type="text" name="Address" value="" size="50" maxlength="125" /></p>
+<p>Gender:<select><option value="Male">Male</option></p>
+<p><option value="Female">Female</option></select></p>
+<p>About Me:</p>
+<p><textarea cols="50" rows="5"></textarea></p>
+
+</body>
+</html>
